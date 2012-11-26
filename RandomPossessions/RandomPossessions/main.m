0400a27deaf739c17f7e686180ddf398f5dd2c4e
Shady <shady@shadyproject.net>
2012-11-26 14:11:32 -0800

    Initial implementation

---
+++ RandomPossessions/RandomPossessions/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by Christopher Martin on 11/25/12.
+//  Copyright (c) 2012 Shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        //create an array
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        //destroy it
+        items = nil;
+    }
+    return 0;
+}
+
