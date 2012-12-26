0c39afb6749681fc76d4bbdefc39d32a8f603fb9
Shady <shady@shadyproject.net>
2012-12-26 11:20:44 -0800

    Removed array to demo weak references

--- RandomPossessions/RandomPossessions/main.m
+++ RandomPossessions/RandomPossessions/main.m
@@ -13,22 +13,19 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        //create an array
-        NSMutableArray *items = [[NSMutableArray alloc] init];
-        
         SPItem *backpack = [[SPItem alloc] init];
         [backpack setItemName:@"Backpack"];
-        [items addObject:backpack];
         
         SPItem *calculator = [[SPItem alloc] init];
         [calculator setItemName:@"Calculator"];
-        [items addObject:calculator];
         
         [backpack setContainedItem:calculator];
         
-       //destroy the array when we're done
-        NSLog(@"Setting items to nil.");    
-        items = nil;
+        backpack = nil;
+        
+        NSLog(@"Container: %@", [calculator container]);
+        
+        calculator = nil;
     }
     return 0;
 }
