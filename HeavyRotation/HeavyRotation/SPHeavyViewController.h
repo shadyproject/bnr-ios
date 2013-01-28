5a6f8fb054a232090b2c4dffd602640382c84da9
Shady <shady@shadyproject.net>
2013-01-28 00:59:31 -0800

    stuff

---
+++ HeavyRotation/HeavyRotation/SPHeavyViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPHeavyViewController.h
+//  HeavyRotation
+//
+//  Created by Christopher Martin on 1/28/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPHeavyViewController : UIViewController
+
+@end
---
+++ HeavyRotation/HeavyRotation/SPHeavyViewController.m
@@ -0,0 +1,38 @@
+//
+//  SPHeavyViewController.m
+//  HeavyRotation
+//
+//  Created by Christopher Martin on 1/28/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPHeavyViewController.h"
+
+@interface SPHeavyViewController ()
+
+@end
+
+@implementation SPHeavyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
