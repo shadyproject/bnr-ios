1b685a7fc9e195726584a933af7e1b2098874291
Shady <shady@shadyproject.net>
2013-02-06 20:11:38 -0800

    Added Item Store

---
+++ Homepwner/Homepwner/SPItemStore.h
@@ -0,0 +1,15 @@
+//
+//  SPItemStore.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 2/6/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPItemStore : NSObject
+
++ (SPItemStore *)sharedStore;
+
+@end
---
+++ Homepwner/Homepwner/SPItemStore.m
@@ -0,0 +1,23 @@
+//
+//  SPItemStore.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 2/6/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPItemStore.h"
+
+@implementation SPItemStore
+
++ (SPItemStore *)sharedStore
+{
+    static SPItemStore *sharedStore = nil;
+    
+    if (!sharedStore) {
+        sharedStore = [[super allocWithZone:nil] init];
+    }
+    
+    return sharedStore;
+}
+@end
