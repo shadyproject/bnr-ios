61a73bf58fc13ea64f4b1ba023377c6b2641e730
Shady <shady@shadyproject.net>
2013-02-12 23:30:30 -0800

    Completed challenge with 2 section table

--- Homepwner/Homepwner/SPItemsViewController.m
+++ Homepwner/Homepwner/SPItemsViewController.m
@@ -13,9 +13,45 @@
 @implementation SPItemsViewController
 #pragma mark -
 #pragma mark UITableViewDataSourceMethods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section)
+    {
+        case 0:
+            return @"Value 50 or below";
+            break;
+            
+        case 1:
+            return @"Value greater than 50";
+            break;
+            
+        default:
+            return @"All Items";
+            break;
+    }
+}
+
+
 - (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)section
 {
-    return [[SPItemStore sharedStore] allItems].count;
+    switch (section) {
+        case 0:
+            return [[SPItemStore sharedStore] itemsWithValueLessThanOrEqualTo:[NSNumber numberWithInt:50]].count;
+            break;
+            
+        case 1:
+            return [[SPItemStore sharedStore] itemsWithValueGreaterThan:[NSNumber numberWithInt:50]].count;
+            
+        default:
+            return [[SPItemStore sharedStore] allItems].count;
+            break;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -28,7 +64,23 @@
                 initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell-Default"];
     }
     
-    SPItem *item = [[[SPItemStore sharedStore] allItems] objectAtIndex:indexPath.row];
+    SPItem *item;
+    
+    switch (indexPath.section) {
+        case 0:
+            item = [[[SPItemStore sharedStore]
+                  itemsWithValueLessThanOrEqualTo:[NSNumber numberWithInt:50]] objectAtIndex:indexPath.row];
+            break;
+            
+        case 1:
+            item = [[[SPItemStore sharedStore]
+                  itemsWithValueGreaterThan:[NSNumber numberWithInt:50]] objectAtIndex:indexPath.row];
+            break;
+            
+        default:
+            item = [[[SPItemStore sharedStore] allItems] objectAtIndex:indexPath.row];
+            break;
+    }
     
     cell.textLabel.text = [item description];
     
