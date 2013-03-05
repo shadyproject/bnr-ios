556fc684a8a527e9497eccc92d6dc60585b5f9ea
Shady <shady@shadyproject.net>
2013-03-04 20:37:21 -0800

    Added Detail View Controller

---
+++ Homepwner/Homepwner/SPDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPDetailViewController.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 3/4/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPDetailViewController : UIViewController
+
+@end
---
+++ Homepwner/Homepwner/SPDetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  SPDetailViewController.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 3/4/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPDetailViewController.h"
+
+@interface SPDetailViewController ()
+
+@end
+
+@implementation SPDetailViewController
+
+@end
