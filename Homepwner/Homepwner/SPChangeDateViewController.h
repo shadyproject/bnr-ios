797a235f9d0062661d5f7d58faf35dc8a4d9dd84
Shady <shady@shadyproject.net>
2013-04-08 21:14:54 -0700

    Added DateChangeControler and xib

---
+++ Homepwner/Homepwner/SPChangeDateViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPChangeDateViewController.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 4/8/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPChangeDateViewController : UIViewController
+
+@end
---
+++ Homepwner/Homepwner/SPChangeDateViewController.m
@@ -0,0 +1,38 @@
+//
+//  SPChangeDateViewController.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 4/8/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPChangeDateViewController.h"
+
+@interface SPChangeDateViewController ()
+
+@end
+
+@implementation SPChangeDateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
