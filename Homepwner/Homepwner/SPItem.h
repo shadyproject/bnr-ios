a44df647eb7d0def89c5d4ef603eea981ebc34a3
Shady <shady@shadyproject.net>
2013-04-08 21:15:14 -0700

    Made dateCreated writable

--- Homepwner/Homepwner/SPItem.h
+++ Homepwner/Homepwner/SPItem.h
@@ -25,6 +25,6 @@
 @property (nonatomic, copy) NSString *itemName;
 @property (nonatomic, copy) NSString *serialNumber;
 @property (nonatomic) int valueInDollars;
-@property (nonatomic, readonly) NSDate *dateCreated;
+@property (nonatomic, copy) NSDate *dateCreated;
 
 @end
