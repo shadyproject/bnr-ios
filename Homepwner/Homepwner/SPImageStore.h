551d00a7f81c4967c8f4a174fe6b6a9f4dd3d4bc
Shady <shady@shadyproject.net>
2013-06-03 22:35:17 -0700

    Initial checkin of SPImageStore

---
+++ Homepwner/Homepwner/SPImageStore.h
@@ -0,0 +1,13 @@
+//
+//  SPImageStore.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 5/28/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPImageStore : NSObject
+
+@end
---
+++ Homepwner/Homepwner/SPImageStore.m
@@ -0,0 +1,13 @@
+//
+//  SPImageStore.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 5/28/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPImageStore.h"
+
+@implementation SPImageStore
+
+@end
