14319c4910cdf6660259588b2019c85008447d0b
Shady <shady@shadyproject.net>
2013-08-13 19:47:33 -0700

    Integrated with image store

--- Homepwner/Homepwner/SPDetailViewController.m
+++ Homepwner/Homepwner/SPDetailViewController.m
@@ -8,7 +8,7 @@
 
 #import "SPDetailViewController.h"
 #import "SPItem.h"
-
+#import "SPImageStore.h"
 
 @implementation SPDetailViewController
 
@@ -32,6 +32,15 @@
     [fmt setTimeStyle:NSDateFormatterNoStyle];
     
     [dateLabel setText:[fmt stringFromDate:item.dateCreated]];
+    
+    NSString* key = item.imageKey;
+    
+    if (key) {
+        UIImage* img = [[SPImageStore sharedStore] imageForKey:item.imageKey];
+        imageView.image = img;
+    } else {
+        imageView.image = nil;
+    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -61,9 +70,25 @@
 #pragma mark -
 #pragma mark ImagePicker Delegates
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    NSString* oldKey = item.imageKey;
+    if (oldKey) {
+        [[SPImageStore sharedStore] deleteImageForKey:oldKey];
+    }
     UIImage *img = [info objectForKey:UIImagePickerControllerOriginalImage];
     
+    CFUUIDRef newImageId = CFUUIDCreate(kCFAllocatorDefault);
+    CFStringRef idString = CFUUIDCreateString(kCFAllocatorDefault, newImageId);
+    //hooray for toll free bridging
+    NSString* key = (__bridge NSString*)idString;
+    
     imageView.image = img;
+    item.imageKey = key;
+    
+    [[SPImageStore sharedStore] setImage:img forKey:item.imageKey];
+    
+    CFRelease(idString);
+    CFRelease(newImageId);
     
     [self dismissViewControllerAnimated:YES completion:nil];
 }
