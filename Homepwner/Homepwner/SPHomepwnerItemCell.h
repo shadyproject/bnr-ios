f1c6557f8e8d9a4e8e44dca9d56aafca2e967019
Shady <shady@shadyproject.net>
2013-08-28 13:30:44 -0700

    boilerplate tablecell class

---
+++ Homepwner/Homepwner/SPHomepwnerItemCell.h
@@ -0,0 +1,13 @@
+//
+//  SPHomepwnerItemCell.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 8/28/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPHomepwnerItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/Homepwner/SPHomepwnerItemCell.m
@@ -0,0 +1,27 @@
+//
+//  SPHomepwnerItemCell.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 8/28/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPHomepwnerItemCell.h"
+
+@implementation SPHomepwnerItemCell
+
+-(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(void)setSelected:(BOOL)selected animated:(BOOL)animated{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
