2cf907dbfc7f3cb68fc18af30652697861b94798
Shady <shady@shadyproject.net>
2013-08-28 13:03:04 -0700

    Added logging around application states

--- Homepwner/Homepwner/SPAppDelegate.m
+++ Homepwner/Homepwner/SPAppDelegate.m
@@ -15,8 +15,9 @@
 
 #pragma mark -
 #pragma mark Application Delegate Notifications
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    DLog(@"%@", NSStringFromSelector(_cmd));
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     // Override point for customization after application launch.
@@ -30,13 +31,13 @@
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+-(void)applicationWillResignActive:(UIApplication *)application{
+    DLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 -(void)applicationDidEnterBackground:(UIApplication *)application{
+    DLog(@"%@", NSStringFromSelector(_cmd));
+    
     BOOL success = [[SPItemStore sharedStore] saveChanges];
     
     if (success) {
@@ -46,19 +47,16 @@
     }
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+-(void)applicationWillEnterForeground:(UIApplication *)application{
+    DLog(@"%@", NSStringFromSelector(_cmd));
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+-(void)applicationDidBecomeActive:(UIApplication *)application{
+    DLog(@"%@", NSStringFromSelector(_cmd));
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+-(void)applicationWillTerminate:(UIApplication *)application{
+    DLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 @end
