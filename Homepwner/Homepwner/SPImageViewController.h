e9b4db4c7faa276784851cf6a9c6ede6fec9b185
Shady <shady@shadyproject.net>
2013-08-29 13:56:56 -0700

    added image view controller

---
+++ Homepwner/Homepwner/SPImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPImageViewController.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 8/29/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPImageViewController : UIViewController
+
+@end
---
+++ Homepwner/Homepwner/SPImageViewController.m
@@ -0,0 +1,38 @@
+//
+//  SPImageViewController.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 8/29/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPImageViewController.h"
+
+@interface SPImageViewController ()
+
+@end
+
+@implementation SPImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
