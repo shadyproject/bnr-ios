caadd1964ad7f3b671af42765f315cad3f64e6dd
Shady <shady@shadyproject.net>
2013-08-29 20:44:08 -0400

    Added stepper cell

---
+++ Homepwner/Homepwner/SPItemStepper.h
@@ -0,0 +1,13 @@
+//
+//  SPItemStepper.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 8/29/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPForwardingTableCell.h"
+
+@interface SPItemStepper : SPForwardingTableCell
+
+@end
---
+++ Homepwner/Homepwner/SPItemStepper.m
@@ -0,0 +1,31 @@
+//
+//  SPItemStepper.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 8/29/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPItemStepper.h"
+
+@implementation SPItemStepper
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
