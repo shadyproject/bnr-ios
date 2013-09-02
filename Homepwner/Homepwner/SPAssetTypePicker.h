928631404ddfa3592ddfa3f7cdd532a39efdff72
Shady <shady@shadyproject.net>
2013-09-02 18:03:56 -0400

    Added SPAssetTypePicker

---
+++ Homepwner/Homepwner/SPAssetTypePicker.h
@@ -0,0 +1,13 @@
+//
+//  SPAssetTypePicker.h
+//  Homepwner
+//
+//  Created by Christopher Martin on 9/2/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPAssetTypePicker : NSObject
+
+@end
---
+++ Homepwner/Homepwner/SPAssetTypePicker.m
@@ -0,0 +1,13 @@
+//
+//  SPAssetTypePicker.m
+//  Homepwner
+//
+//  Created by Christopher Martin on 9/2/13.
+//  Copyright (c) 2013 Shadyproject. All rights reserved.
+//
+
+#import "SPAssetTypePicker.h"
+
+@implementation SPAssetTypePicker
+
+@end
