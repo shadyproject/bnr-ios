4b70e5e3747f3958bbbed285434891b2e5a800de
Shady <shady@shadyproject.net>
2013-09-01 23:24:11 -0400

    addred thumbnailForImage method

--- Homepwner/Homepwner/SPItem.m
+++ Homepwner/Homepwner/SPItem.m
@@ -21,4 +21,38 @@
 @dynamic orderingValue;
 @dynamic assetType;
 
+-(void)setThumbnailFromImage:(UIImage *)image{
+    CGSize origImgSize = [image size];
+    
+    CGRect thumbRect = CGRectMake(0, 0, 40, 40);
+    
+    // figure out a scaling ratio
+    float ratio = MAX(thumbRect.size.width/origImgSize.width, thumbRect.size.height/origImgSize.height);
+    
+    //create a new context for our thumbnail
+    UIGraphicsBeginImageContextWithOptions(thumbRect.size, NO, 0.0);
+    
+    //rounded rect
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:thumbRect cornerRadius:5.0];
+    [path addClip];
+    
+    //center the image
+    CGRect projectRect;
+    projectRect.size.width = ratio * origImgSize.width;
+    projectRect.size.height = ratio * origImgSize.height;
+    projectRect.origin.x = (thumbRect.size.width - projectRect.size.width)/2;
+    projectRect.origin.y = (thumbRect.size.height - projectRect.size.height)/2;
+    
+    //draw it in the context
+    [image drawInRect:projectRect];
+    
+    UIImage *smallImg = UIGraphicsGetImageFromCurrentImageContext();
+    [self setThumbnail:smallImg];
+    
+    NSData *data = UIImagePNGRepresentation(smallImg);
+    [self setThumbnailData:data];
+    
+    UIGraphicsEndImageContext();
+}
+
 @end
